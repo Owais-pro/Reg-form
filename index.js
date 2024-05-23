1d2ef51e91191875ba11a5d9cadfbd6d9ed548ec
Rao-Owais <rao.owais2020@gmail.com>
2024-05-23 11:48:30 +0500

    completed

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ form.addEventListener('submit', (event) =>{
     let cnicRegex = /^\d{5}-\d{7}-\d$/;
     let ageRegex = /^(1[9-9]|[2-5][0-9]|60)$/;
 
+    if(usernameRegex.test(Name.value) && emailRegex.test(Email.value) && passwordRegex.test( Password.value) && cnicRegex.test(CNIC.value) && ageRegex.test(Age.value)){
+        console.log(Name.value)
+        console.log(Email.value)
+        console.log(Password.value)
+        console.log(CNIC.value)
+        console.log(Age.value)
+    }
+    else{
+        console.log("Enter UserName min 4 & max 14 character long, Number & Special Character Not Allowed");
+        console.log("Enter Valid Email Includes eg: @gmail.com");
+        console.log("Enter 8 Character Long Password Includes One Special & Capital Letter");
+        console.log("Enter CNIC In A Format: XXXXX-XXXXXXX-X");
+        console.log("Enter Age between 18 to 60");
 
-
-
-
+    }
 })
